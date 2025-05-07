747eae81722684bda9ed5f925ed1e7e45e71531b
Aythar <albadyaythar@gmail.com>
2025-05-07 05:24:26 +0300

    Create app.js

---
+++ app/app/app/app.js
@@ -0,0 +1,8 @@
+document.getElementById('app').innerHTML = `
+  <button onclick="startGame()">ابدأ اللعبة</button>
+`;
+
+function startGame() {
+  alert("تم بدء لعبة الشطرنج!");
+  // هنا يمكننا ربط تطبيق الشطرنج مع الكود الذي تم تطويره سابقًا
+}
